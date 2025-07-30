fe400f32579e8afb5b1041791e6d7ab264cfc4eb
Ethan Cunningham <ethan@sunrise.co.za>
2025-07-30 16:06:33 +0400

    Added recipe API

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jk4i9xp)lfj^$-#se8id04vyhpp7ku@p%br&5q!k97-^_dvo(+"
+SECRET_KEY = (
+    "django-insecure-jk4i9xp)lfj^$-#se8id04vyhpp7ku@p%br&5q!k97-^_dvo(+"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "drf_spectacular",
     "user",
+    "recipe",
 ]
 
 MIDDLEWARE = [
